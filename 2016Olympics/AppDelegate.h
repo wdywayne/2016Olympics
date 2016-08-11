a2c598155cc763e2430fb3b1167e5ede0d0a1f71
apple <apple@appledemacbook-air.local>
2016-08-11 11:05:47 +0800

    Initial Commit

---
+++ 2016Olympics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2016Olympics
+//
+//  Created by apple on 16/8/11.
+//  Copyright © 2016年 wdywayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2016Olympics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2016Olympics
+//
+//  Created by apple on 16/8/11.
+//  Copyright © 2016年 wdywayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
